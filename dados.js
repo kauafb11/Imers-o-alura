c55c2b7821d6ee94b9fd939d4d7b5dd3ec748e96
kauafb11 <128090965+kauafb11@users.noreply.github.com>
2024-09-08 17:49:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,35 @@
+let dados = [
+  
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate fada fadinha sk8 "
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é um dos grandes nomes do judô brasileiro.",
+      link:"https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judô judo judoca judoka "
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e beleza. Campeã olímpica e mundial, ela é um dos maiores nomes da ginástica artística.",
+      link:"https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags:"ginástica ginstica ouro"
+    },
+    {
+      titulo: "Hugo Calderano",
+      descricao: "Hugo Calderano é um tenista de mesa brasileiro que se destaca no cenário mundial. Com sua habilidade e agilidade, ele conquistou diversos títulos e é um dos principais nomes do tênis de mesa brasileiro.",
+      link: "https://pt.wikipedia.org/wiki/Hugo_Calderano",
+      tags: "tenis de mesas ping-pong"
+    },
+    {
+      titulo: "Gabriel Medina",
+      descricao: "Gabriel Medina é um surfista brasileiro que conquistou o mundo com suas manobras radicais. Campeão mundial, ele é um dos maiores nomes do surf mundial.",
+      link:"https://pt.wikipedia.org/wiki/Gabriel_Medina",
+      tags: "surf"
+    }
+  ];
+
+
